5e163b793bf69a28d808e7d693d4be03056e4ed7
riwu <riwu0730@gmail.com>
2017-09-14 16:41:28 +0800

    Add reproduce duration screen

--- src/ReproduceDuration.js
+++ src/ReproduceDuration.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'react-router-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,11 +9,11 @@ const styles = StyleSheet.create({
     color: 'white',
     marginTop: 100,
     marginBottom: 10,
+    marginLeft: 20,
+    marginRight: 20,
     fontSize: 25,
     fontWeight: 'bold',
-  },
-  round: {
-    color: 'white',
+    textAlign: 'center',
   },
   instructionContainer: {
     flexDirection: 'row',
@@ -23,6 +22,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightgrey',
     marginTop: 30,
     marginBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
     borderRadius: 10,
     borderWidth: 10,
     borderColor: 'lightgrey',
@@ -37,44 +38,46 @@ const styles = StyleSheet.create({
   text: {
     textAlign: 'center',
   },
-  readyButton: {
-    color: 'blue',
-    textAlign: 'center',
-    backgroundColor: 'white',
-    padding: 5,
-    margin: 20,
-    width: 300,
+  button: {
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    width: 120,
   },
-  plus: {
-    color: 'white',
-    fontSize: 50,
-    marginTop: 50,
+  buttonText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
 
-const ReproduceDuration = () => (
-  <View style={styles.container}>
-    <Text style={styles.ready}>READY FOR YOUR TASK?</Text>
-    <Text style={styles.round}>ROUND 1 OF 5</Text>
-    <Text style={styles.plus} >+</Text>
-    <View style={styles.instructionContainer}>
-      <View style={{ flex: 0.1 }} />
-      <View style={styles.instructions}>
-        <Text style={styles.tellUs}>Tell us how long the screen turns red for</Text>
-        <Text style={styles.text}>1. Click 'ready' and the screen will turn red.</Text>
-        <Text style={styles.text}>2. When the screen is red:</Text>
-        <Text style={styles.text}>- Focus on the cross above(+)</Text>
-        <Text style={styles.text}>- <Text style={{ textDecorationLine: 'underline' }}>Don't count</Text> how much time has passed</Text>
-        <Text style={styles.text}>- We're interested in what it
-            <Text style={{ fontStyle: 'italic' }}> feels like</Text> to you
-          </Text>
+class ReproduceDuration extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.ready}>HOW LONG WAS THE SCREEN RED FOR?</Text>
+        <View style={styles.instructionContainer}>
+          <View style={{ flex: 0.1 }} />
+          <View style={styles.instructions}>
+            <Text style={styles.tellUs}>Reproduce the duration</Text>
+            <Text style={styles.text}>1. Press 'START' when you're ready</Text>
+            <Text style={styles.text}>2. Press 'STOP' when you think you've reproduced the duration the screen was red for.</Text>
+            <Text style={styles.text}>Remember:</Text>
+            <Text style={styles.text}>- <Text style={{ textDecorationLine: 'underline' }}>Don't count</Text> how much time has passed</Text>
+            <Text style={styles.text}>- We're interested in what it
+                <Text style={{ fontStyle: 'italic' }}> feels like</Text> to you
+              </Text>
+          </View>
+          <View style={{ flex: 0.1 }} />
+        </View>
+        <TouchableHighlight style={[styles.button, { backgroundColor: '#00e500' }]}>
+          <Text style={styles.buttonText}>START TIMER</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={[styles.button, { backgroundColor: 'red' }]}>
+          <Text style={styles.buttonText}>STOP TIMER</Text>
+        </TouchableHighlight>
       </View>
-      <View style={{ flex: 0.1 }} />
-    </View>
-    <Link to="/ReadyTransition">
-      <Text style={styles.readyButton}>Ready</Text>
-    </Link>
-  </View>
-  );
+    );
+  }
+}
 
 export default ReproduceDuration;
