628c4895c9db013bad2c4ac6adcfec829dcf5226
riwu <riwu0730@gmail.com>
2017-09-23 11:52:09 +0800

    add react-native-simple-radio-button package src for editing

--- src/questions/Question.js
+++ src/questions/Question.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import RadioForm, { RadioButtonInput, RadioButton } from 'react-native-simple-radio-button';
+import RadioForm from '../react-native-simple-radio-button';
 import Button from '../Button';
 
 const styles = StyleSheet.create({
