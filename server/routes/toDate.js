b33aaf20471e8e53cbffc09fa94f0898a8f4f604
riwu <riwu0730@gmail.com>
2017-10-21 11:02:32 +0800

    Offset to SG time

--- server/routes/toDate.js
+++ server/routes/toDate.js
@@ -1,4 +1,4 @@
 const moment = require('moment');
 
-const toDate = time => moment(time).format('YYYY-MM-DD HH:mm:ss');
+const toDate = time => moment(time).utcOffset(8).format('YYYY-MM-DD HH:mm:ss');
 module.exports = toDate;
