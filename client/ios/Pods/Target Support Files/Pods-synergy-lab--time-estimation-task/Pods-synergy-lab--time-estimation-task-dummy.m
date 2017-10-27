753f5d38d6a9536c9018e3925c280364888747ca
riwu <riwu0730@gmail.com>
2017-10-27 20:34:18 +0800

    pod install

---
+++ client/ios/Pods/Target Support Files/Pods-synergy-lab--time-estimation-task/Pods-synergy-lab--time-estimation-task-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_synergy_lab__time_estimation_task : NSObject
+@end
+@implementation PodsDummy_Pods_synergy_lab__time_estimation_task
+@end
