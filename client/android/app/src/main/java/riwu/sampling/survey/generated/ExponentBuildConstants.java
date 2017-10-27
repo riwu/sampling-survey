c81b0a6d29847f3fb0b55257dcc066e1b39ded1e
riwu <riwu0730@gmail.com>
2017-10-27 21:36:53 +0800

    update android version

--- client/android/app/src/main/java/riwu/sampling/survey/generated/ExponentBuildConstants.java
+++ client/android/app/src/main/java/riwu/sampling/survey/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package riwu.sampling.survey.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp9d67daaf49924ef3bb8710f4bf65523b://localhost:80";
 
 }
