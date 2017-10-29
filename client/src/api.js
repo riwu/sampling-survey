ed1abc0db66edb18eff4af974deac14990187a13
riwu <riwu0730@gmail.com>
2017-10-29 11:03:16 +0800

    Still use expo deviceID for iOS to continue data collection from older group

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,7 @@
 import Frisbee from 'frisbee';
 import { Constants } from 'expo';
 import DeviceInfo from 'react-native-device-info';
+import { Platform } from 'react-native';
 
 const API_BASE_URL = 'http://13.228.235.195:3002/';
 // const API_BASE_URL = 'http://localhost:3002/';
@@ -22,7 +23,7 @@ let deviceId;
 let country;
 let isTablet;
 try {
-  deviceId = DeviceInfo.getUniqueID(); // those who haven't update from App Store won't have it
+  deviceId = (Platform.OS === 'ios') ? Constants.deviceId : DeviceInfo.getUniqueID(); // those who haven't update from App Store won't have it
   country = DeviceInfo.getTimezone();
   isTablet = DeviceInfo.isTablet();
 } catch (e) {
