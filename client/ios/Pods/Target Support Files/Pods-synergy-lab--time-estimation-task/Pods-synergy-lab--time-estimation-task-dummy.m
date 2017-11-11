0df3847d96c90bb50f199a2edc263ac8c7982d34
riwu <riwu0730@gmail.com>
2017-11-11 22:43:00 +0800

    remove android and ios folder

--- client/ios/Pods/Target Support Files/Pods-synergy-lab--time-estimation-task/Pods-synergy-lab--time-estimation-task-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_synergy_lab__time_estimation_task : NSObject
-@end
-@implementation PodsDummy_Pods_synergy_lab__time_estimation_task
-@end
