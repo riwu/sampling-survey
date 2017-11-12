528cb463886b9475058086e47189e5b118e7adc5
riwu <riwu0730@gmail.com>
2017-11-13 00:41:26 +0800

    cancel notification and set badge count to 0 at get data

--- client/src/misc/GetData.js
+++ client/src/misc/GetData.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import { Alert } from 'react-native';
 import { connect } from 'react-redux';
+import Notifications from 'react-native-push-notification';
 import MiddleText from '../components/MiddleText';
 import api from '../actions/api';
 import { setCode } from '../actions';
 
-const GetData = ({ state, ...props }) => (
-  <MiddleText
-    text="Please enable Internet to allow data to be collected."
-    nextScene="RewardScreen"
-    noPrevious
-    onPress={() => api.postAll(state).then((code) => {
-      console.log('res', code);
-      props.setCode(code);
-      return false;
-    }).catch((err) => {
-      console.log('error', err.message);
-      Alert.alert('Error', `Failed to send data.\n${err.message || 'Make sure you have Internet connection!'}`);
-      return err;
-    })}
-  />
-);
+class GetData extends React.Component {
+  componentDidMount() {
+    Notifications.setApplicationIconBadgeNumber(0);
+    Notifications.cancelAllLocalNotifications();
+  }
+
+  render() {
+    const { state, ...props } = this.props;
+    return (
+      <MiddleText
+        text="Please enable Internet to allow data to be collected."
+        nextScene="RewardScreen"
+        noPrevious
+        onPress={() => api.postAll(state).then((code) => {
+          console.log('res', code);
+          props.setCode(code);
+          return false;
+        }).catch((err) => {
+          console.log('error', err.message);
+          Alert.alert('Error', `Failed to send data.\n${err.message || 'Make sure you have Internet connection!'}`);
+          return err;
+        })}
+      />
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   state,
