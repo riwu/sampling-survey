46a34c081b83475d694134226b8b41b7a6c751e9
riwu <riwu0730@gmail.com>
2017-12-23 22:07:43 +0800

    update api endpoint to use env var

--- client/src/actions/api.js
+++ client/src/actions/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import DeviceInfo from 'react-native-device-info';
 
-axios.defaults.baseURL = 'https://wangriwu.com:3002/';
-// axios.defaults.baseURL = 'http://192.168.1.187:3002/';
+axios.defaults.baseURL = `${process.env.SAMPLING_API_URL}/`;
 
 const deviceId = DeviceInfo.getUniqueID();
 
@@ -11,7 +10,7 @@ const deviceInfo = {
   deviceName: DeviceInfo.getDeviceName(),
   isDevice: !DeviceInfo.isEmulator(),
   version: DeviceInfo.getVersion(),
-  timezone: (new Date()).getTimezoneOffset(),
+  timezone: new Date().getTimezoneOffset(),
   country: DeviceInfo.getTimezone(),
   isTablet: DeviceInfo.isTablet(),
 };
@@ -20,34 +19,36 @@ console.log('info', deviceInfo);
 
 const get = path => axios.get(path).then(response => response.data);
 
-const [patch, put] = ['patch', 'put'].map(method =>
-  (path, payload) => axios({
+const [patch, put] = ['patch', 'put'].map(method => (path, payload) =>
+  axios({
     method,
     url: path,
     data: { ...payload, deviceId },
-  }).then(response => response.data)
+  })
+    .then(response => response.data)
     .catch((err) => {
       console.log('encountered error for', path, ':', (err.response || {}).data);
       throw new Error((err.response || {}).data);
-    }));
+    }),
+);
 
 export default {
   postDevice: () => put('device', deviceInfo),
   postAnswer: (answer) => {
     console.log('posting answer', answer);
-    return put('answer', answer)
-      .catch(e => console.log('Post answer api', e, answer));
+    return put('answer', answer).catch(e => console.log('Post answer api', e, answer));
   },
-  postSchedule: schedule => put('experiment', { schedule })
-    .catch(e => console.log('Post experiment schedule', e, schedule)),
-  postTrial: answer => put('trial', answer)
-    .catch(e => console.log('Post trial', e, answer)),
-  postExperimentStarted: answer => patch('experiment/started', answer)
-    .catch(e => console.log('Post experiment start', e, answer)),
-  postExperimentAnswer: answer => put('experiment/answer', answer)
-    .catch(e => console.log('Post experiment ans', e, answer)),
-  postExperimentRound: answer => put('experiment/round', answer)
-    .catch(e => console.log('Post experiment round', e, answer)),
+  postSchedule: schedule =>
+    put('experiment', { schedule }).catch(e =>
+      console.log('Post experiment schedule', e, schedule),
+    ),
+  postTrial: answer => put('trial', answer).catch(e => console.log('Post trial', e, answer)),
+  postExperimentStarted: answer =>
+    patch('experiment/started', answer).catch(e => console.log('Post experiment start', e, answer)),
+  postExperimentAnswer: answer =>
+    put('experiment/answer', answer).catch(e => console.log('Post experiment ans', e, answer)),
+  postExperimentRound: answer =>
+    put('experiment/round', answer).catch(e => console.log('Post experiment round', e, answer)),
   isDisqualified: () => get(`disqualified/${deviceId}`),
   postAll: state => put('all', { ...state, device: deviceInfo }),
   disqualify: () => patch('disqualify'),
