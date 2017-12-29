97becc92ad1b94ab6744a14ecc21ba2d051c2283
riwu <riwu0730@gmail.com>
2017-12-29 21:55:13 +0800

    update reward screen message

--- client/src/misc/RewardScreen.js
+++ client/src/misc/RewardScreen.js
@@ -5,11 +5,13 @@ import getResponseRate from './getResponseRate';
 
 const RewardScreen = ({ meetCriteria, code }) => (
   <MiddleText
-    text={meetCriteria
-      ? `${'Thank you for completing our experimental study.\n\n' +
-        'Please input the following code into MTurk for your reimbursement.\n\nCode: '}${code}`
-      : 'You did not meet the requirement of 80% response rate for our task.\n\n' +
-        'We regret to inform you that you will not be reimbursed for your participation.'}
+    text={
+      meetCriteria
+        ? `${'Thank you for completing our experimental study.\n\nCode: '}${code}\n\n` +
+          'Send the above code and your full name to phsnkmy@nus.edu.sg for reimbursement.'
+        : 'You did not meet the requirement of 80% response rate for our task.\n\n' +
+          'We regret to inform you that you will not be reimbursed for your participation.'
+    }
   />
 );
 
