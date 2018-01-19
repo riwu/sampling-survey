46c466ec6eac90207fbecad140e3e0e15f7848f5
riwu <riwu0730@gmail.com>
2018-01-19 22:24:16 +0800

    remove eligibility check

--- client/src/questionnaire/isEligible.js
+++ client/src/questionnaire/isEligible.js
@@ -1,13 +1,14 @@
 const isBetween = (value, min, max) => value >= min && value <= max;
 
-const isEligible = (answers) => {
-  const relationship = answers['QUESTION 5'];
-  return ((relationship.index === 0) || (relationship.index === 1 &&
-      !['3-6 months', '>6 months'].includes(relationship[1]))) &&
-    answers['QUESTION 2'].index !== 2 &&
-    isBetween(Number(answers['QUESTION 3'][-1]), 21, 35) &&
-    answers['QUESTION 4'].index === 0;
-};
+const isEligible = answers =>
+  true
+  // const relationship = answers['QUESTION 5'];
+  // return ((relationship.index === 0) || (relationship.index === 1 &&
+  //     !['3-6 months', '>6 months'].includes(relationship[1]))) &&
+  //   answers['QUESTION 2'].index !== 2 &&
+  //   isBetween(Number(answers['QUESTION 3'][-1]), 21, 35) &&
+  //   answers['QUESTION 4'].index === 0;
+;
 
 export const isSingle = state => state.answers['QUESTION 5'].index === 0;
 
