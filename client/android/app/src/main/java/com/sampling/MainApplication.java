58570e209d1953e5a5e069c376cd5ea3661ede48
riwu <riwu0730@gmail.com>
2018-02-27 23:05:55 +0800

    add react-native-code-push

--- client/android/app/src/main/java/com/sampling/MainApplication.java
+++ client/android/app/src/main/java/com/sampling/MainApplication.java
@@ -3,6 +3,7 @@ package com.sampling;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
             new ReactNativePushNotificationPackage(),
             new RNDeviceInfo()
       );
