e52d164971a8a7158d951b070648c69d30973fae
riwu <riwu0730@gmail.com>
2018-03-01 17:41:22 +0800

    check for notification setting and trigger alert if disabled on app start

--- client/src/RoutingScreen.js
+++ client/src/RoutingScreen.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import Notifications from 'react-native-push-notification';
+import { Platform, Linking, Alert } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
+import NotificationSettingsAndroid from 'react-native-permission-settings';
+import OpenAppSettings from 'react-native-app-settings';
 import getMatchingSchedule from './experiment/getMatchingSchedule';
+
+const showAlert = openSettings =>
+  Alert.alert(
+    'App notification is turned off',
+    "Please enable notification to ensure that you don't miss sessions",
+    [
+      {
+        text: 'Close',
+      },
+      {
+        text: 'Open Settings',
+        onPress: openSettings,
+      },
+    ],
+  );
+
+const checkPermissions = () => {
+  if (Platform.OS === 'ios') {
+    Notifications.checkPermissions(({ alert }) => {
+      if (!alert) {
+        showAlert(() => Linking.openURL('app-settings:'));
+      }
+    });
+  } else {
+    NotificationSettingsAndroid.areNotificationsEnabled((isEnabled) => {
+      if (!isEnabled) {
+        showAlert(() => OpenAppSettings.open());
+      }
+    });
+  }
+};
 
 class RoutingScreen extends React.Component {
   componentWillMount() {
@@ -21,6 +55,7 @@ class RoutingScreen extends React.Component {
       route = 'NotEligible';
     } else if (Object.keys(props.schedule).length > 0 && props.route !== 'RewardScreen') {
       route = getMatchingSchedule(props.schedule, props.route);
+      checkPermissions();
     }
     console.log('replacing route', route);
     Actions.replace(route);
