ab50f65b0f5f9c24ea50359fbfc1e02e8b4092ce
riwu <riwu0730@gmail.com>
2018-03-01 17:19:22 +0800

    add react-native-app-settings for android

--- client/android/app/src/main/java/com/sampling/MainApplication.java
+++ client/android/app/src/main/java/com/sampling/MainApplication.java
@@ -3,6 +3,7 @@ package com.sampling;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.krazylabs.OpenAppSettingsPackage;
 import com.alienslab.permissionsettings.PermissionSettingsPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new OpenAppSettingsPackage(),
             new PermissionSettingsPackage(),
             new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
             new ReactNativePushNotificationPackage(),
