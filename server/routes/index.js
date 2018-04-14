a5ce3dbbfab300208df2880f1f4dc77f316e59fd
riwu <riwu0730@gmail.com>
2018-04-14 17:22:11 +0800

    fix experiment order and write only last 49 experiment into csv

--- server/routes/index.js
+++ server/routes/index.js
@@ -267,7 +267,13 @@ router.post('/answers', authenticate, async (req, res) => {
     };
   });
 
-  const roundsPromise = query.getRounds().each((row) => {
+  const roundsPromise = query.getRounds();
+
+  await answersPromise;
+  await experimentAnswersPromise;
+  // wait after experimentAnswersPromise to ensure schedule order is correct,
+  // as if round insertion is performed first the order will be wrong if experiment has no round
+  await roundsPromise.each((row) => {
     const {
       deviceId, round, schedule, ...durations
     } = row;
@@ -290,9 +296,6 @@ router.post('/answers', authenticate, async (req, res) => {
     };
   });
 
-  await answersPromise;
-  await experimentAnswersPromise;
-  await roundsPromise;
   res.send(data);
 });
 
