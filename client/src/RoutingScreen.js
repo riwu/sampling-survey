7de39c764bb74859a6b0b20bdbecd759c35d9f64
riwu <riwu0730@gmail.com>
2018-04-20 20:01:51 +0800

    update checkPermissions for Android

--- client/src/RoutingScreen.js
+++ client/src/RoutingScreen.js
@@ -3,41 +3,36 @@ import Notifications from 'react-native-push-notification';
 import { Platform, Linking, Alert } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
-import NotificationSettingsAndroid from 'react-native-permission-settings';
 import OpenAppSettings from 'react-native-app-settings';
 import getMatchingSchedule, { schedule } from './experiment/getMatchingSchedule';
 import { postAll, experimentStarted } from './actions';
 import { FIRST_EXPERIMENT_ROUTE } from './constants';
 
-const showAlert = openSettings =>
-  Alert.alert(
-    'App notification is turned off',
-    "Please enable notification to ensure that you don't miss sessions",
-    [
-      {
-        text: 'Close',
-      },
-      {
-        text: 'Open Settings',
-        onPress: openSettings,
-      },
-    ],
-  );
-
 const checkPermissions = () => {
-  if (Platform.OS === 'ios') {
-    Notifications.checkPermissions(({ alert }) => {
-      if (!alert) {
-        showAlert(() => Linking.openURL('app-settings:'));
-      }
-    });
-  } else {
-    NotificationSettingsAndroid.areNotificationsEnabled((isEnabled) => {
-      if (!isEnabled) {
-        showAlert(() => OpenAppSettings.open());
-      }
-    });
-  }
+  Notifications.checkPermissions(({ alert }) => {
+    console.log('Notification permission', alert);
+    if (!alert) {
+      Alert.alert(
+        'App notification is turned off',
+        "Please enable notification to ensure that you don't miss sessions",
+        [
+          {
+            text: 'Close',
+          },
+          {
+            text: 'Open Settings',
+            onPress: () => {
+              if (Platform.OS === 'ios') {
+                Linking.openURL('app-settings:');
+              } else {
+                OpenAppSettings.open();
+              }
+            },
+          },
+        ],
+      );
+    }
+  });
 };
 
 class RoutingScreen extends React.Component {
